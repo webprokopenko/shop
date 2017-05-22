36a2225d8628b62b02c03db315bad04b88469a5d
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-22 18:25:11 +0300

    Added global backend access control

--- backend/config/main.php
+++ backend/config/main.php
@@ -52,5 +52,15 @@ return [
         ],
 
     ],
+    'as access' => [
+                'class' => 'yii\filters\AccessControl',
+                'except' => ['site/login', 'site/error'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
     'params' => $params,
 ];
