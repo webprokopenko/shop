cd65266c80fb00d5bdb14689160a0d0136bc26ab
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-23 09:07:29 +0300

    Separated UrlManagers

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -1,3 +1,7 @@
 <?php
 return [
+    'cookieValidationKey' => '',
+    'cookieDomain' => '.shop.dev',
+    'frontendHostInfo' => 'http://shop.dev',
+    'backendHostInfo' => 'http://backend.shop.dev',
 ];
---
+++ frontend/config/urlManager.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'urlManager' => 'yii\web\UrlManager',
+    'hostInfo' => $params['frontendHostInfo'],
+    'enablePrettyUrl' => true,
+        'showScriptName' => false,
+        'rules' => [
+            '' => 'site/index',
+            '<_a:about|contact|signup|login>' => 'site/<_a>',
+            '<_c:[\w\-]+>' => '<_c>/index',
+            '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+            '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+            '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+    ],
+];
