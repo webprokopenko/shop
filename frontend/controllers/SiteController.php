61449bce3c77b18fc44e6422ad66a48071874948
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-24 19:57:43 +0300

    Added contact service

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -14,6 +14,7 @@ use frontend\forms\SignupForm;
 use frontend\forms\ContactForm;
 use frontend\services\auth\SignupService;
 use frontend\services\auth\PasswordResetService;
+use frontend\services\contact\ContactService;
 
 /**
  * Site controller
@@ -21,16 +22,20 @@ use frontend\services\auth\PasswordResetService;
 class SiteController extends Controller
 {
     private $passwordResetService;
+    private $contactService;
 
-    public function __construct($id, $module, PasswordResetService $passwordResetService, $config = [])
+    public function __construct(
+        $id,
+        $module,
+        PasswordResetService $passwordResetService,
+        ContactService $contactService,
+        $config = [])
     {
         parent::__construct($id, $module, $config);
         $this->passwordResetService = $passwordResetService;
+        $this->contactService = $contactService;
     }
 
-    /**
-     * @inheritdoc
-     */
     public function behaviors()
     {
         return [
@@ -124,19 +129,21 @@ class SiteController extends Controller
      */
     public function actionContact()
     {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
+        $form = new ContactForm();
+        if ($form->load(Yii::$app->request->post()) && $form->validate()) {
+            try {
+                $this->contactService->send($form);
                 Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
-            } else {
+                return $this->goHome();
+            } catch (\Exception $e) {
+                Yii::$app->errorHandler->logException($e);
                 Yii::$app->session->setFlash('error', 'There was an error sending your message.');
             }
-
             return $this->refresh();
         }
 
         return $this->render('contact', [
-            'model' => $model,
+            'model' => $form,
         ]);
     }
 
--- frontend/tests/unit/forms/ContactFormTest.php
+++ frontend/tests/unit/forms/ContactFormTest.php
@@ -6,7 +6,7 @@ use frontend\forms\ContactForm;
 
 class ContactFormTest extends \Codeception\Test\Unit
 {
-    public function testSendEmail()
+    public function testSuccess()
     {
         $model = new ContactForm();
 
@@ -16,17 +16,6 @@ class ContactFormTest extends \Codeception\Test\Unit
             'subject' => 'very important letter subject',
             'body' => 'body of current message',
         ];
-
-        expect_that($model->sendEmail('admin@example.com'));
-
-        // using Yii2 module actions to check email was sent
-        $this->tester->seeEmailIsSent();
-
-        $emailMessage = $this->tester->grabLastSentEmail();
-        expect('valid email is sent', $emailMessage)->isInstanceOf('yii\mail\MessageInterface');
-        expect($emailMessage->getTo())->hasKey('admin@example.com');
-        expect($emailMessage->getFrom())->hasKey('tester@example.com');
-        expect($emailMessage->getSubject())->equals('very important letter subject');
-        expect($emailMessage->toString())->contains('body of current message');
+        expect_that($model->validate());
     }
 }
