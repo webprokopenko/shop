7350e19e756d23161a084950fdbd65f5b12d47bf
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-24 13:07:17 +0300

    Unified Cache bag fixed

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -17,6 +17,7 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
+            'cookieValidationKey' => $params['cookieValidationKey'],
         ],
         'user' => [
             'identityClass' => 'common\models\User',
@@ -28,8 +29,11 @@ return [
             ],
         ],
         'session' => [
-            // this is the name of the session cookie used for login on the frontend
-            'name' => 'advanced-frontend',
+            'name' => '_session',
+            'cookieParams' => [
+                'domain' => $params['cookieDomain'],
+                'httpOnly' => true,
+            ]
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
