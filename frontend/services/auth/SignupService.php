6b295ba001356842ccf9ebe3679f7c318dc52f94
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-24 21:21:18 +0300

    Extracted storage methods

--- frontend/services/auth/SignupService.php
+++ frontend/services/auth/SignupService.php
@@ -22,9 +22,7 @@ class SignupService
             $form->password
         );
 
-        if (!$user->save()) {
-            throw new \RuntimeException('Saving error.');
-        }
+        $this->save($user);
 
         $sent = $this->mailer
             ->compose(
@@ -45,15 +43,20 @@ class SignupService
             throw new \DomainException('Empty confirm token.');
         }
 
-        /* @var $user User */
-        $user = User::findOne(['email_confirm_token' => $token]);
-
-        if (!$user) {
+        $user = $this->getByEmailConfirmToken($token);
+        $user->confirmSignup();
+        $this->save($user);
+    }
+    private function getByEmailConfirmToken(string $token): User
+    {
+        if (!$user = User::findOne(['email_confirm_token' => $token])) {
             throw new \DomainException('User is not found.');
         }
+        return $user;
+    }
 
-        $user->confirmSignup();
-
+    private function save(User $user): void
+    {
         if (!$user->save()) {
             throw new \RuntimeException('Saving error.');
         }
