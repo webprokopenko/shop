4cf0286ba83cd1bc78406446e6438863d258fcd5
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-24 18:56:28 +0300

    Added user constructor

--- common/entities/User.php
+++ common/entities/User.php
@@ -23,9 +23,21 @@ use yii\web\IdentityInterface;
  */
 class User extends ActiveRecord implements IdentityInterface
 {
+    use InstantiateTrait;
+    
     const STATUS_DELETED = 0;
     const STATUS_ACTIVE = 10;
 
+    public function __construct(string $username, string $email, string $password)
+    {
+        $this->username = $username;
+        $this->email = $email;
+        $this->setPassword($password);
+        $this->created_at = time();
+        $this->status = self::STATUS_ACTIVE;
+        $this->generateAuthKey();
+        parent::__construct();
+    }
 
     /**
      * @inheritdoc
