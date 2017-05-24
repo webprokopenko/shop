c4e09bd4eb4db960b04a98865df72de93292a5ab
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-24 19:50:30 +0300

    Added password reset DI

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -1,12 +1,18 @@
 <?php
+
 namespace common\bootstrap;
 
+use frontend\services\auth\UserPasswordResetService;
 use yii\base\BootstrapInterface;
 
 class SetUp implements BootstrapInterface
 {
     public function bootstrap($app)
     {
+        $container = \Yii::$container;
 
+        $container->setSingleton(UserPasswordResetService::class, [], [
+            [$app->params['supportEmail'] => $app->name . ' robot'],
+        ]);
     }
 }
--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -20,6 +20,14 @@ use frontend\services\auth\PasswordResetService;
  */
 class SiteController extends Controller
 {
+    private $passwordResetService;
+
+    public function __construct($id, $module, PasswordResetService $passwordResetService, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->passwordResetService = $passwordResetService;
+    }
+
     /**
      * @inheritdoc
      */
@@ -172,7 +180,7 @@ class SiteController extends Controller
         $form = new PasswordResetRequestForm();
         if ($form->load(Yii::$app->request->post()) && $form->validate()) {
             try {
-                (new PasswordResetService())->request($form);
+                $this->passwordResetService->request($form);
                 Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
                 return $this->goHome();
             } catch (\DomainException $e) {
@@ -195,10 +203,9 @@ class SiteController extends Controller
      */
     public function actionResetPassword($token)
     {
-        $service = new PasswordResetService();
 
         try {
-            $service->validateToken($token);
+            $this->passwordResetService->validateToken($token);
         } catch (\DomainException $e) {
             throw new BadRequestHttpException($e->getMessage());
         }
@@ -206,7 +213,7 @@ class SiteController extends Controller
         $form = new ResetPasswordForm();
         if ($form->load(Yii::$app->request->post()) && $form->validate()) {
             try {
-                (new PasswordResetService())->reset($token, $form);
+                $this->passwordResetService->reset($token, $form);
                 Yii::$app->session->setFlash('success', 'New password saved.');
             } catch (\DomainException $e) {
                 Yii::$app->errorHandler->logException($e);
