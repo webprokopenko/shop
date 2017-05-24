6e34683acf377bedff3ac68b0ac229b0ac7884e2
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-24 20:02:22 +0300

    Removed support email

--- frontend/services/auth/PasswordResetService.php
+++ frontend/services/auth/PasswordResetService.php
@@ -9,12 +9,7 @@ use Yii;
 
 class PasswordResetService
 {
-    private $supportEmail;
 
-    public function __construct($supportEmail)
-    {
-        $this->supportEmail = $supportEmail;
-    }
 
     public function request(PasswordResetRequestForm $form): void
     {
