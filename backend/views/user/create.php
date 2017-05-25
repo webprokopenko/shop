9701701c76d8d5f3164fbf29ef738d8ee0d5d044
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-25 17:09:40 +0300

    Added user create action

--- backend/views/user/create.php
+++ backend/views/user/create.php
@@ -1,17 +1,24 @@
 <?php
 
+use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 
-
-/* @var $this yii\web\View */
-/* @var $model shop\entities\User */
 
 $this->title = 'Create User';
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <div class="user-create">
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxLength' => true]) ?>
+    <?= $form->field($model, 'email')->textInput(['maxLength' => true]) ?>
+    <?= $form->field($model, 'password')->passwordInput(['maxLength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
 </div>
