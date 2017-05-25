18336247b1059f4c9cfdf12e0a9385da3c329cd5
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-25 16:36:32 +0300

    Remove unused h1 on backend user views

--- backend/views/user/update.php
+++ backend/views/user/update.php
@@ -11,11 +11,7 @@ $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' 
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="user-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
-
 </div>
