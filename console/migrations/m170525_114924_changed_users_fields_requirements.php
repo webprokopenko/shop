2e1a84db1a8563d2d156e775856f497e7c8b7625
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-25 14:51:13 +0300

    Add table user_networks

---
+++ console/migrations/m170525_114924_changed_users_fields_requirements.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\db\Migration;
+
+class m170525_114924_changed_users_fields_requirements extends Migration
+{
+    public function up()
+    {
+        $this->alterColumn('{{%users}}', 'username', $this->string());
+        $this->alterColumn('{{%users}}', 'password_hash', $this->string());
+        $this->alterColumn('{{%users}}', 'email', $this->string());
+    }
+
+    public function down()
+    {
+        $this->alterColumn('{{%users}}', 'username', $this->string()->notNull());
+        $this->alterColumn('{{%users}}', 'password_hash', $this->string()->notNull());
+        $this->alterColumn('{{%users}}', 'email', $this->string()->notNull());
+    }
+}
