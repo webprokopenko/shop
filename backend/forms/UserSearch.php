eec389bd5fe2a839c242511e4777ff6feebbf607
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-25 16:47:15 +0300

    Added user date search

--- backend/forms/UserSearch.php
+++ backend/forms/UserSearch.php
@@ -10,7 +10,8 @@ use shop\entities\User\User;
 class UserSearch extends User
 {
     public $id;
-    public $created_at;
+    public $date_from;
+    public $date_to;
     public $username;
     public $email;
     public $status;
@@ -18,8 +19,9 @@ class UserSearch extends User
     public function rules()
     {
         return [
-            [['id', 'status', 'created_at'], 'integer'],
+            [['id', 'status'], 'integer'],
             [['username', 'email'], 'safe'],
+            [['date_from', 'date_to'], 'date', 'format' => 'php:Y-m-d'],
         ];
     }
 
@@ -47,12 +49,14 @@ class UserSearch extends User
         $query->andFilterWhere([
             'id' => $this->id,
             'status' => $this->status,
-            'created_at' => $this->created_at,
         ]);
 
         $query
             ->andFilterWhere(['like', 'username', $this->username])
-            ->andFilterWhere(['like', 'email', $this->email]);
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['>=', 'created_at', $this->date_from ? strtotime($this->date_from . ' 00:00:00') : null])
+            ->andFilterWhere(['<=', 'created_at', $this->date_to ? strtotime($this->date_to . ' 23:59:59') : null]);
+
 
         return $dataProvider;
     }
