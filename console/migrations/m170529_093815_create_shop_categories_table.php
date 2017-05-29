6a1758038f635c9bb57693e9946fe8fd0b41707a
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-29 12:43:40 +0300

    Added migrate table shop_categories

---
+++ console/migrations/m170529_093815_create_shop_categories_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `shop_categories`.
+ */
+class m170529_093815_create_shop_categories_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%shop_categories}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'slug' => $this->string()->notNull(),
+            'title' => $this->string(),
+            'description' => $this->string(),
+            'meta_json' => 'JSON NOT NULL',
+            'lft' => $this->integer()->notNull(),
+            'rgt' => $this->integer()->notNull(),
+            'depth' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->insert('{{%shop_categories}}', [
+            'id' => 1,
+            'name' => '',
+            'slug' => 'root',
+            'title' => null,
+            'description' => null,
+            'meta_json' => '{}',
+            'lft' => 1,
+            'rgt' => 2,
+            'depth' => 0,
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%shop_categories}}');
+    }
+}
