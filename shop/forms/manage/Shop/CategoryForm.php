68f8f31b378631f8c2cf8da6cf92f48f7e8215d3
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-05-29 19:24:40 +0300

    Added categories management

--- shop/forms/manage/Shop/CategoryForm.php
+++ shop/forms/manage/Shop/CategoryForm.php
@@ -6,6 +6,7 @@ use shop\entities\Shop\Category;
 use shop\forms\CompositeForm;
 use shop\forms\manage\MetaForm;
 use shop\validators\SlugValidator;
+use yii\helpers\ArrayHelper;
 
 /**
  * @property MetaForm $meta;
@@ -47,7 +48,12 @@ class CategoryForm extends CompositeForm
             [['name', 'slug'], 'unique', 'targetClass' => Category::class, 'filter' => $this->_category ? ['<>', 'id', $this->_category->id] : null]
         ];
     }
-
+    public function parentCategoriesList(): array
+    {
+        return ArrayHelper::map(Category::find()->orderBy('lft')->asArray()->all(), 'id', function (array $category) {
+            return ($category['depth'] > 1 ? str_repeat('-- ', $category['depth'] - 1) . ' ' : '') . $category['name'];
+        });
+    }
     public function internalForms(): array
     {
         return ['meta'];
